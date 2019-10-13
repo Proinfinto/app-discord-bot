4078587d10f857dbe5116ebbe8671846281e8828
proinfinto <furkanyaran@hotmail.com>
2019-10-14 00:27:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if(msg.member.user.tag === 'irb#8939'){
+    msg.reply('Sen sus çomar!');
+  }
+});
+
+client.login(token);
